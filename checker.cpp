062eb6e6a5f6d5ed4dc225225ff3e295aafc7f1c
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-27 15:30:31 +0530

    Split the function

--- checker.cpp
+++ checker.cpp
@@ -1,15 +1,43 @@
 #include <assert.h>
+#include<iostream>
+
+using namespace std;
+const float bpmLimit[] = {70 , 150};
+const float spo2Bar = 90;
+const float respRateLimit[] = {30 , 95};
+
+bool checkBPM(float bpm){
+	bool retval=true;
+	if(bpm < bpmLimit[0] || bpm > bpmLimit[1]) {
+	cout<<"BPM abnormal"<<endl;
+    retval= false;
+  }
+  
+  return retval;
+}
+
+bool checkspo2(float spo2){
+	bool retval=true;
+	if(spo2 < spo2Bar) {
+	cout<<"Oxygen saturation low!"<<endl;
+    retval = false;
+  }
+  return retval;
+}
+
+bool checkRespRate(float respRate){
+	bool retval = true;
+	if(respRate < respRateLimit[0] || respRate > respRateLimit[1]) {
+	cout<<"Respiration rate abnormal"<<endl;
+    retval = false;
+  }
+  return retval;
+}
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+  return (checkBPM(bpm)&&checkspo2(spo2)&&checkRespRate(respRate));
 }
+
 
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);

ba5efec82c984a0fa6b33ee38076c49f12439faf
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-27 16:35:28 +0530

    Update

--- checker.cpp
+++ checker.cpp
@@ -3,57 +3,37 @@
 
 using namespace std;
 const float bpmLimit[] = {70 , 150};
-const float spo2Bar = 90;
+const float spo2Bar[] = {90 , 100};
 const float respRateLimit[] = {30 , 95};
 
-bool isBPMNormal(float bpm){
+
+bool vitalIsNormal(float vitalValue , float lower, float upper){
 	bool retval=true;
-	if(bpm < bpmLimit[0]) {
-	cout<<"BPM Very Low"<<endl;
+	if(vitalValue <= lower) {
+	cout<<"Very Low "<<vitalValue<<endl;
     retval= false;
   }
-  if(bpm > bpmLimit[1]){
-  	cout<<"BPM Very high"<<endl;
+  else if(vitalValue >= upper){
+  	cout<<"Very high "<<vitalValue<<endl;
   	retval = false;
   }
-  
-  return retval;
-}
-
-bool isspo2Normal(float spo2){
-	bool retval=true;
-	if(spo2 < spo2Bar) {
-	cout<<"Oxygen saturation low!"<<endl;
-    retval = false;
-  }
-  return retval;
-}
-
-bool isRespRateNormal(float respRate){
-	bool retval = true;
-	if(respRate < respRateLimit[0]) {
-	cout<<"Respiration rate low"<<endl;
-    retval = false;
-  }
-  if(respRate > respRateLimit[1]){
-  	cout<<"Respiration Rate High"<<endl;
-  	retval = false;
+  else{
+  	cout<<"Normal "<<vitalValue<<endl;
   }
   return retval;
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (checkBPM(bpm)&&checkspo2(spo2)&&checkRespRate(respRate));
+  return (vitalIsNormal(bpm , bpmLimit[0], bpmLimit[1]) && vitalIsNormal(spo2 , spo2Bar[0], spo2Bar[1]) && vitalIsNormal(respRate , respRateLimit[0],respRateLimit[1]));
 }
 
-
 int main() {
+	cout<<"1 Testing 'vitalsAreOK()' Method: "<<endl;
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
-  assert(checkBPM(78) == true);
-  assert(checkBPM(165) == false);
-  assert(checkspo2(98) == true);
-  assert(checkspo2(85) == false);
-  assert(checkRespRate(87) == true);
-  assert(checkRespRate(96) == false);
+  	cout<<"\nTesting 'vitalIsOK()' Method: "<<endl;
+  assert(vitalIsNormal(80, bpmLimit[0],bpmLimit[1]) == true);
+  assert(vitalIsNormal(87, spo2Bar[0],spo2Bar[1]) == false);
+  assert(vitalIsNormal(98, respRateLimit[0],respRateLimit[1]) == false);
+  
 }

e150d6a4edb4177e86dba4cd3454a2697650ebff
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-27 18:33:37 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,39 +1,89 @@
-#include <assert.h>
 #include<iostream>
-
+#include<assert.h>
 using namespace std;
-const float bpmLimit[] = {70 , 150};
-const float spo2Bar[] = {90 , 100};
-const float respRateLimit[] = {30 , 95};
 
+const int n_vitals = 3; //3 In this case.
+typedef struct Vitals{
+	string vitalName="";
+	float vitalValue;
+	float lower,upper;
+}vital;
+vital allVitals[n_vitals];
+
+void initializeVitalsInformation(){
+	int loopCounter=0;
+	string name;
+	float lower, upper;
+	for(loopCounter=0;loopCounter<n_vitals;loopCounter++){
+		cin>>name>>lower>>upper;
+		allVitals[loopCounter].vitalValue = 0; //initially - before any real input
+		allVitals[loopCounter].vitalName = name;
+		allVitals[loopCounter].lower = lower;
+		allVitals[loopCounter].upper = upper;
+		cout<<allVitals[loopCounter].vitalName<<endl;
+	}
+
+}
+
+void getCurrentVitalValues(){
+	int loopCounter=0;
+	float value;
+	for(loopCounter = 0; loopCounter < n_vitals; loopCounter++){
+		cin>>value;
+		allVitals[loopCounter].vitalValue = value;
+	}
+}
 
 bool vitalIsNormal(float vitalValue , float lower, float upper){
 	bool retval=true;
 	if(vitalValue <= lower) {
-	cout<<"Very Low "<<vitalValue<<endl;
-    	retval= false;
+	cout<<"Very Low with value : "<<vitalValue<<endl;
+    retval= false;
   }
   else if(vitalValue >= upper){
-  	cout<<"Very high "<<vitalValue<<endl;
+  	cout<<"Very high with value : "<<vitalValue<<endl;
   	retval = false;
   }
   else{
-  	cout<<"Normal "<<vitalValue<<endl;
+  	cout<<"Normal with value : "<<vitalValue<<endl;
   }
   return retval;
 }
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (vitalIsNormal(bpm , bpmLimit[0], bpmLimit[1]) && vitalIsNormal(spo2 , spo2Bar[0], spo2Bar[1]) && vitalIsNormal(respRate , respRateLimit[0],respRateLimit[1]));
+bool vitalsAreNormal(){
+	int loopCounter = 0;
+	bool status = true;
+	for(loopCounter=0;loopCounter<n_vitals;loopCounter++){
+		cout<<allVitals[loopCounter].vitalName<<" status -  ";
+		bool new_status = vitalIsNormal(allVitals[loopCounter].vitalValue , allVitals[loopCounter].lower , allVitals[loopCounter].upper);
+		status = status && new_status;
+	}
+	return status;
 }
+int main(){
+	initializeVitalsInformation(); // one time thing
+	//Check if all vitals are in normal range
+	int loopCounter=0;     
+	getCurrentVitalValues(); //This needs to be called continuously hard coding initially
+	//bool status = vitalsAreNormal();
+		/*
+		allVitals[0].vitalValue = 72; 
+		allVitals[0].vitalName = "bpm";
+		allVitals[0].lower = 70;
+		allVitals[0].upper = 150;
+		
+		allVitals[1].vitalValue = 95; 
+		allVitals[1].vitalName = "SpO2";
+		allVitals[1].lower = 90;
+		allVitals[1].upper = 100;
+		
+		allVitals[2].vitalValue = 65; 
+		allVitals[2].vitalName = "RespRate";
+		allVitals[2].lower = 30;
+		allVitals[2].upper = 70;
+		*/
+	assert(vitalsAreNormal() == true);
+	assert(vitalsAreNormal() == false);
 
-int main() {
-	cout<<"1 Testing 'vitalsAreOK()' Method: "<<endl;
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
-  	cout<<"\nTesting 'vitalIsOK()' Method: "<<endl;
-  assert(vitalIsNormal(80, bpmLimit[0],bpmLimit[1]) == true);
-  assert(vitalIsNormal(87, spo2Bar[0],spo2Bar[1]) == false);
-  assert(vitalIsNormal(98, respRateLimit[0],respRateLimit[1]) == false);
-  
+	return 0;
 }

d74782ca87a6feeb32ddc71ba42f431300957d28
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-28 10:25:39 +0530

    update

--- vitals_main.cpp
+++ vitals_main.cpp
@@ -36,14 +36,14 @@ int main(){
 	int loopCounter = 0;
 	Initialize();
 	char c;
-	//int n_times_while_loop_runs = 5; // Don't know the terminating condition yet. While loop runs continuously until device shuts down.
-	while((c=getchar())!='E'){ 
+	int n_times_while_loop_runs = 3; // Don't know the terminating condition yet. While loop runs continuously until device shuts down.
+	while(n_times_while_loop_runs){ 
 		for(loopCounter = 0; loopCounter < n_vitals; loopCounter++){
 			allVitals[loopCounter].setCurrentValue();
 		}
 		bool status = vitalsAreNormal();
 		
-		//n_times_while_loop_runs--;
+		n_times_while_loop_runs--;
 		//cout<<endl<<status<<endl;
 	}
 	//Add tests for vitalIsOk() and vitalsAreOk() - 3 for each
